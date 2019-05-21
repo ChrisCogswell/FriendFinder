af302e67f5e391d39f48e9a2fdca029c8f160dcd
ChrisCogs <ccogs@yahoo.com>
2019-05-20 21:33:43 -0400

    Adjusted survey.html and apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,10 +11,9 @@ module.exports = function(app) {
      
     app.post("/api/friends", function(req, res) {
 
-        if(friendData.length > 1) {
             friendData.push(req.body);
             res.json(true);
-        }
+        
         // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
         // It will do this by sending out the value "true" have a table
         // req.body is available since we're using the body parsing middleware
